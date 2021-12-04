8306c50345e99747c41abacaa970bb40c9226010
DevRyu <ryuseonghyeon@lyuseonghyeon-ui-MacBookPro.local>
2021-12-05 00:37:52 +0900

    add getProductById unit test

--- controller/products.js
+++ controller/products.js
@@ -1,20 +1,31 @@
-const productModel = require('../models/Product');
+const productModel = require("../models/Product");
 
 exports.createProduct = async (req, res, next) => {
-    try {
-        const createdProduct = await productModel.create(req.body);
-        res.status(201).json(createdProduct);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const createdProduct = await productModel.create(req.body);
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 exports.getProducts = async (req, res, next) => {
-    try {
-        const allProducts = await productModel.find({});
-        res.status(200).json(allProducts);
-    } catch (error) {
-        next(error);
+  try {
+    const allProducts = await productModel.find({});
+    res.status(200).json(allProducts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductById = async (req, res, next) => {
+  try {
+    const product = await productModel.findById(req.params.productId);
+    if (product) {
+      return res.status(200).json(product);
     }
-}
+    res.status(404).send();
+  } catch (error) {
+    next(error);
+  }
+};
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,8 +1,8 @@
-const productController = require('../../controller/products');
-const productModel = require('../../models/Product');
-const httpMocks = require('node-mocks-http');
-const newProduct = require('../data/new-product.json');
-const allProducts = require('../data/all-products.json');
+const productController = require("../../controller/products");
+const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const newProduct = require("../data/new-product.json");
+const allProducts = require("../data/all-products.json");
 
 productModel.create = jest.fn();
 productModel.find = jest.fn();
@@ -10,77 +10,112 @@ productModel.findById = jest.fn();
 productModel.findByIdAndUpdate = jest.fn();
 productModel.findByIdAndDelete = jest.fn();
 
-const productId = "5diijfdsijdfhuehwufwe";
-const updatedProduct = { name: "updated name", description: "updated description" };
+const productId = "1232414";
+const updatedProduct = {
+  name: "updated name",
+  description: "updated description",
+};
 let req, res, next;
 beforeEach(() => {
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = jest.fn();
-})
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = jest.fn();
+});
 
 describe("Product Controller Create", () => {
-    beforeEach(() => {
-        req.body = newProduct;
-    })
-    it("should have a createProduct funtcion", () => {
-        expect(typeof productController.createProduct).toBe("function")
-    })
-    it("should call ProductModel.create", async () => {
-        await productController.createProduct(req, res, next);
-        expect(productModel.create).toBeCalledWith(newProduct);
-    })
-    it("should return 201 response code", async () => {
-        await productController.createProduct(req, res, next);
-        expect(res.statusCode).toBe(201);
-        expect(res._isEndCalled()).toBeTruthy();
-    })
-    it("should return json body in response", async () => {
-        productModel.create.mockReturnValue(newProduct)
-        await productController.createProduct(req, res, next);
-        expect(res._getJSONData()).toStrictEqual(newProduct)
-    })
-    it("should handle errors", async () => {
-        // 에러메시지를 만드는 부분
-        const errorMessage = { message: "description property missing" };
-        const rejectedPromise = Promise.reject(errorMessage);
-        
-        // 에러메시지를 받아서 처리하는 부분
-        productModel.create.mockReturnValue(rejectedPromise);
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+  it("should have a createProduct funtcion", () => {
+    expect(typeof productController.createProduct).toBe("function");
+  });
+  it("should call ProductModel.create", async () => {
+    await productController.createProduct(req, res, next);
+    expect(productModel.create).toBeCalledWith(newProduct);
+  });
+  it("should return 201 response code", async () => {
+    await productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+  it("should return json body in response", async () => {
+    productModel.create.mockReturnValue(newProduct);
+    await productController.createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+  });
+  it("should handle errors", async () => {
+    // 에러메시지를 만드는 부분
+    const errorMessage = { message: "description property missing" };
+    const rejectedPromise = Promise.reject(errorMessage);
 
-        // next는 에러를 처리하기 때문에 next에 expecter를 넣어준다.
-        await productController.createProduct(req, res, next);
-        expect(next).toBeCalledWith(errorMessage);
-    })
-})
+    // 에러메시지를 받아서 처리하는 부분
+    productModel.create.mockReturnValue(rejectedPromise);
 
+    // next는 에러를 처리하기 때문에 next에 expecter를 넣어준다.
+    await productController.createProduct(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
+  });
+});
 
 describe("productController Get", () => {
-    it("should have a getProducts function", () => {
-        expect(typeof productController.getProducts).toBe("function")
-    })
+  it("should have a getProducts function", () => {
+    expect(typeof productController.getProducts).toBe("function");
+  });
 
-    it("should call ProductModel.find({})", async () => {
-        await productController.getProducts(req, res, next);
-        expect(productModel.find).toHaveBeenCalledWith({});
-    })
+  it("should call ProductModel.find({})", async () => {
+    await productController.getProducts(req, res, next);
+    expect(productModel.find).toHaveBeenCalledWith({});
+  });
 
-    it("should return 200 statusCode ", async () => {
-        await productController.getProducts(req, res, next);
-        expect(res.statusCode).toBe(200);
-        expect(res._isEndCalled).toBeTruthy();
-    })
-    it("should return allProducts in json body ", async () => {
-        productModel.find.mockReturnValue(allProducts);
-        await productController.getProducts(req, res, next);
-        expect(res._getJSONData()).toStrictEqual(allProducts)
-    })
-    it("should handle errors", async () => {
-        const errorMessage = { message: "error finding product data" };
-        const rejectedPromise = Promise.reject(errorMessage);       
-        productModel.find.mockReturnValue(rejectedPromise);
-        await productController.getProducts(req, res, next);
-        expect(next).toBeCalledWith(errorMessage);
-    })
+  it("should return 200 statusCode ", async () => {
+    await productController.getProducts(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+  it("should return allProducts in json body ", async () => {
+    productModel.find.mockReturnValue(allProducts);
+    await productController.getProducts(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(allProducts);
+  });
+  it("should handle errors", async () => {
+    const errorMessage = { message: "error finding product data" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    productModel.find.mockReturnValue(rejectedPromise);
+    await productController.getProducts(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
+  });
+});
 
-})
+describe("productController GetProductById", () => {
+  it("should have a getProductById function", () => {
+    expect(typeof productController.getProductById).toBe("function");
+  });
+
+  it("should call ProductModel.findById(productId)", async () => {
+    req.params.productId = productId;
+    await productController.getProductById(req, res, next);
+    expect(productModel.findById).toBeCalledWith(req.params.productId);
+  });
+
+  it("should return json body and 200 statusCode ", async () => {
+    req.params.productId = productId;
+    productModel.findById.mockReturnValue(newProduct);
+    await productController.getProductById(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled()).toBeTruthy();
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+  });
+  it("should return 404 if id not exis", async () => {
+    productModel.findById.mockReturnValue(null);
+    await productController.getProductById(req, res, next);
+    expect(res.statusCode).toBe(404);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+  it("should handle error", async () => {
+    const errorMessage = { message: "error finding product data" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    productModel.findById.mockReturnValue(rejectedPromise);
+    await productController.getProductById(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
+  });
+});
