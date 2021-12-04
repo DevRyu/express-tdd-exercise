2f773318b3eaefdc49d9be414dc7a324c7b797bc
DevRyu <ryuseonghyeon@lyuseonghyeon-ui-MacBookPro.local>
2021-12-05 00:45:06 +0900

    add getProductById integration test

--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -27,4 +27,18 @@ it("GET /api/products", async () => {
   expect(Array.isArray(response.body)).toBeTruthy();
   expect(response.body[0].name).toBeDefined();
   expect(response.body[0].description).toBeDefined();
+  firstProduct = response.body[0];
+});
+
+it("GET /api/products/:productId", async () => {
+  const response = await request(app).get("/api/products/" + firstProduct._id);
+  expect(response.statusCode).toBe(200);
+  expect(response.body.name).toBe(firstProduct.name);
+  expect(response.body.description).toBe(firstProduct.description);
+});
+it("GET id dose not exist /api/products/:productId", async () => {
+  const response = await request(app).get(
+    "/api/products/" + "61ab7827df4e88cb8f13fbb2"
+  );
+  expect(response.statusCode).toBe(404);
 });
